8a9e0dfd6e5e8476ca2c098d3e641595be1fc4cb
Zachariah Magee <103608503+zachariahmagee@users.noreply.github.com>
2025-02-09 10:56:59 -0800

    Merged dev to master

--- src/zm/penguin/interactions/Scrollbar.java
+++ src/zm/penguin/interactions/Scrollbar.java
@@ -1,41 +1,76 @@
 package zm.penguin.interactions;
 
 import zm.penguin.components.Component;
+import zm.penguin.utils.Orientation;
+
+import static zm.penguin.styles.Theme.*;
 
 import static processing.core.PApplet.*;
 
-class ScrollBar extends Component {
+public class ScrollBar extends Component {
     //int l, w, t, h;
+//    Component container;
+//    int offset;
     int totalElements;
     int totalLength;
     int startPosition;
     int mouseOffset;
     float movementScaler;
-    boolean orientation;
     boolean inverted;
     boolean active;
-    boolean locked = false;
+    public boolean locked = false;
+    public Orientation orientation;
 
     static public final boolean HORIZONTAL = true;
     static public final boolean VERTICAL = false;
     static public final boolean INVERT = true;
     static public final boolean NORMAL = false;
 
-    public ScrollBar(boolean orientation, boolean inverted) {
+    public ScrollBar(Orientation orientation, boolean inverted, Component container) {
         this.orientation = orientation;
         this.inverted = inverted;
         this.mouseOffset = 0;
         this.active = false;
+
+        this.f = scrollbar_bg;
+        this.s = scrollbar;
+
+        this.container = container;
+
+        this.onMouseEnter = () -> { app.cursor(HAND); };
+        this.onMouseExit = () -> { app.cursor(ARROW); };
+
     }
 
-    public void draw() {}
+    @Override
+    public void draw() {
+//        println(mouseInside);
+
+        app.rectMode(CORNER);
+        app.noStroke();
+
+
+        if (orientation.isVertical()) {
+            app.fill(scrollbar_bg);
+            app.rect(l, container.t, w, container.h);
+
+            app.fill(scrollbar);
+            app.rect(l, t, w, h, 5, 5, 5, 5);
+        } else {
+            app.fill(scrollbar_bg);
+            app.rect(container.l, t, totalLength,h);
+
+            app.fill(scrollbar);
+            app.rect(l, t + 1, w, h, 5,5,5,5);
+        }
+    }
 
     @Override
     public String toString() {
-        return "";
+        return "Scrollbar: " + orientation;
     }
 
-    boolean active() {
+    public boolean active() {
         return this.active;
     }
 
@@ -46,18 +81,21 @@ class ScrollBar extends Component {
     public void update(int totalElements, int totalLength, int newL, int newT, int newW, int newH) {
         this.totalElements = totalElements;
         this.totalLength = totalLength;
-        if (orientation == VERTICAL) this.mouseOffset += (newT + newH) - (t + h);
+        if (orientation.isVertical()) this.mouseOffset += (newT + newH) - (t + h);
         else this.mouseOffset += (newL + newW) - (l + w);
         this.l = newL;
         this.w = newW;
         this.t = newT;
         this.h = newH;
+        this.r = l + w;
+        this.b = t + h;
         this.movementScaler = (totalElements / (float) totalLength);
     }
 
     public boolean mouseOver(int x, int y) {
+//        println(container.getClass(),x,y,l,t,w,h,mouseInside);
         if ((x >= l) && (x <= l + w) && (y >= t) && (y <= t + h)) {
-            if (orientation == VERTICAL) {
+            if (orientation.isVertical()) {
                 mouseOffset = t + h - y;
                 startPosition = y;
             } else {
@@ -72,17 +110,17 @@ class ScrollBar extends Component {
     }
 
     public int move(int x, int y, int currentScroll, int minScroll, int maxScroll) {
-        int mainCoord = y;
-        if (orientation == HORIZONTAL) mainCoord = x;
+        int mainCoordinate = y;
+        if (!orientation.isVertical()) mainCoordinate = x;
 
-        int currentPosition = mainCoord + mouseOffset;
+        int currentPosition = mainCoordinate + mouseOffset;
         int elementsMoved = (int) ((currentPosition - (startPosition + mouseOffset)) * movementScaler);
 
         if (abs(elementsMoved) >= 1) {
             if (inverted) elementsMoved = -elementsMoved;
             if ( ( (elementsMoved < 0) && (currentScroll == minScroll) )
                     || ((elementsMoved > 0) && (currentScroll == maxScroll)) ) {
-                if (orientation == VERTICAL) {
+                if (orientation.isVertical()) {
                     mouseOffset = t + h - y;
                     startPosition = y;
                 } else {
@@ -93,7 +131,7 @@ class ScrollBar extends Component {
                 currentScroll += elementsMoved;
                 if (currentScroll < minScroll) currentScroll = minScroll;
                 else if (currentScroll > maxScroll) currentScroll = maxScroll;
-                startPosition = mainCoord;
+                startPosition = mainCoordinate;
             }
         }
         return currentScroll;
